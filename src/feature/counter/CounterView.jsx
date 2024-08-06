925a56c1956ee214d637a218b0cf4519b9659760
RashedujjamanNoor <noorrk042@gmail.com>
2024-08-06 23:01:19 +0600

    facing node_modules problem

---
+++ src/feature/counter/CounterView.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "./counterSlice";
+
+const CounterView = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default CounterView;
